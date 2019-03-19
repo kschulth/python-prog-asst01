bbe60aa4872451b4f89ad883be1feb17f0658920
kschulth <47764280+kschulth@users.noreply.github.com>
2019-03-19 17:15:52 -0600

    Add files via upload

--- task1.py
+++ task1.py
@@ -1,12 +1,36 @@
 for i in range(10):
     print(i)
-
 for r in range(500):
     print(r)
-
 for k in range(21):
     print(k)
-
 l=[4,5,6]
 for i in l:
     print(i)
+i=8
+while i>0:
+    print(i)
+    i=i-1
+m=12
+while m>9:
+    print(m)
+    m=m-5
+i=8
+while i>0:
+    print(i)
+    break
+    i=i-1
+i = 8-10
+if i>0:
+    print("i is greater then zero")
+else:
+    print("i is less then zero")
+# the bubble sort uses, def,if,print, greater than values, and print functions
+# the function print is already defined in the python the others are defined in his functions
+# there are 4 loops
+#looks like 7 values nested
+#line 28
+# no arguments
+#its called by a def function
+#it prints the digits out
+#
